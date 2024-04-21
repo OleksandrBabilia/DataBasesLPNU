0286dd8cd9b80905a784780af36edcf13de5fc26
Oleksandr Babilia <oleksandrbabilia@Oleksandrs-MacBook-Pro.local>
2024-04-21 22:46:33 +0300

    Added: Books Model

--- backend/books/models.py
+++ backend/books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from authors.models import Author
+from publishers.models import Publisher
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=55)
+    isbn = models.CharField(max_length=13)
+    publication_date = models.DateField()
+    price = models.FloatField()
+    author_id = models.ManyToManyField(Author)
+    publisher_id = models.ForeignKey(Publisher, on_delete=models.DO_NOTHING)
+    cover = models.ImageField()
+    about = models.TextField()
+
